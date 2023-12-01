57cb5472dfca49a5d20174096f6e7e5258deacf2
mike mambwe <mikeapple056@gmail.com>
2023-12-01 19:22:41 +0200

    the fix

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,2 @@
-// config.js
-export const PORT = 5555;
-//the export file
-export const mongoDBURL =
-  "mongodb+srv://root:<password>@book-store-mern.3tqx6xr.mongodb.net/?retryWrites=true&w=majority";
-//please  create a free database.
+export const PORT = 3000;
+
