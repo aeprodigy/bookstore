9d8aef697821b4943d0e8b781ea54d7c857dfb10
mike mambwe <mikeapple056@gmail.com>
2023-12-01 17:46:12 +0200

    this

--- backend/config.js
+++ backend/config.js
@@ -2,5 +2,5 @@
 export const PORT = 5555;
 //the export file
 export const mongoDBURL =
-  "mongodb+srv://root:root@book-store-mern.3tqx6xr.mongodb.net/books-collection?retryWrites=true&w=majority";
+  "mongodb+srv://root:<password>@book-store-mern.3tqx6xr.mongodb.net/?retryWrites=true&w=majority";
 //please  create a free database.
